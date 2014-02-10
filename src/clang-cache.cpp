3bdcb2b55b1a01ac0aa7862ee867fadbca006aa3
Stephane Sezer <sas@cd80.net>
2014-02-09 16:35:38 -0800

    Run clang with the passed arguments.

--- src/clang-cache.cpp
+++ src/clang-cache.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <string.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char **argv)
 {
-  std::cout << "Le caching daemon, hon, hon hon!" << std::endl;
-  return 0;
+  int separator = 0;
+  char** compile_argv;
+
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "--") == 0) {
+      separator = i;
+      break;
+    }
+  }
+
+  compile_argv = argv + separator;
+  compile_argv[0] = separator >= 2 ? argv[separator - 1] : strdup("clang");
+
+  return execvp(compile_argv[0], compile_argv);
 }
