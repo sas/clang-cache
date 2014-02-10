45928f29a5996def30f1e6c0ceb78a05b37c6d0d
Stephane Sezer <sas@cd80.net>
2014-02-09 17:19:34 -0800

    Use C++ includes when appropriate.

--- src/clang-cache.cpp
+++ src/clang-cache.cpp
@@ -1,8 +1,8 @@
 #include "daemon.h"
 
+#include <cstring>
 #include <err.h>
 #include <iostream>
-#include <string.h>
 #include <unistd.h>
 
 int main(int argc, char **argv)
@@ -11,14 +11,14 @@ int main(int argc, char **argv)
   char** compile_argv;
 
   for (int i = 1; i < argc; ++i) {
-    if (strcmp(argv[i], "--") == 0) {
+    if (std::strcmp(argv[i], "--") == 0) {
       separator = i;
       break;
     }
   }
 
   compile_argv = argv + separator;
-  compile_argv[0] = separator >= 2 ? argv[separator - 1] : strdup("clang");
+  compile_argv[0] = separator >= 2 ? argv[separator - 1] : (char*) "clang";
 
   if (clc::daemon::start()) {
     /* Send compile message to server. */
