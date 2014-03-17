e5d667e67526b797ae8d8723739193f29af5626e
Stephane Sezer <sas@cd80.net>
2014-03-16 19:09:28 -0700

    Remove useless logger calls.

--- src/server/clc_service.cpp
+++ src/server/clc_service.cpp
@@ -11,16 +11,12 @@ namespace clc { namespace server {
 void clc_service::register_compilation(const std::vector<std::string>& argv,
                                        const std::string& cwd)
 {
-  LOG_INFO() << __FUNCTION__;
-
   cache_.fill(argv, cwd);
 }
 
 void clc_service::find_definition(clc::rpc::cursor& ret,
                                   const std::string& usr)
 {
-  LOG_INFO() << __FUNCTION__;
-
   auto sl = cache_.find_definition(usr);
 
   ret.file = sl.path;
