f2c6548b3e3970ff31a79b80398dfec7c840ed19
Xavier Deguillard <deguilx@gmail.com>
2014-03-24 23:31:11 -0700

    Remove copy/pasta in client.cpp

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -17,12 +17,12 @@ namespace ttransport = apache::thrift::transport;
 
 namespace clc { namespace client {
 
-void register_compilation(int argc, char** argv)
+static clc::rpc::clc_ifClient* connect_to_server(void)
 {
   auto socket = boost::make_shared<ttransport::TSocket>(utils::sock_path());
   auto transport = boost::make_shared<ttransport::TBufferedTransport>(socket);
   auto protocol = boost::make_shared<tprotocol::TCompactProtocol>(transport);
-  clc::rpc::clc_ifClient client(protocol);
+  auto client = new clc::rpc::clc_ifClient(protocol);
 
   do {
     try {
@@ -38,37 +38,32 @@ void register_compilation(int argc, char** argv)
     }
   } while (!transport->isOpen());
 
+  return client;
+}
+
+void register_compilation(int argc, char** argv)
+{
+  auto client = connect_to_server();
+  auto transport = client->getOutputProtocol()->getTransport();
+
   std::vector<std::string> cmd(argv, argv + argc);
-  client.register_compilation(cmd, utils::cwd());
+  client->register_compilation(cmd, utils::cwd());
   transport->close();
+
+  delete client;
 }
 
 void find_definition(const char* usr)
 {
-  /* XXX: Bad copy-pasta, need to clean this up. */
-  auto socket = boost::make_shared<ttransport::TSocket>(utils::sock_path());
-  auto transport = boost::make_shared<ttransport::TBufferedTransport>(socket);
-  auto protocol = boost::make_shared<tprotocol::TCompactProtocol>(transport);
-  clc::rpc::clc_ifClient client(protocol);
-
-  do {
-    try {
-      /*
-       * When the server is being launched, we might end-up here before
-       * the server has created the unix socket. Let's loop until it has
-       * been, this shouldn't be long.
-       */
-      transport->open();
-    } catch (ttransport::TTransportException& e) {
-      /* I wonder if that is a bug in thrift. */
-      transport->close();
-    }
-  } while (!transport->isOpen());
+  auto client = connect_to_server();
+  auto transport = client->getOutputProtocol()->getTransport();
 
   rpc::cursor ret;
   std::string arg(usr);
-  client.find_definition(ret, arg);
+  client->find_definition(ret, arg);
   transport->close();
+
+  delete client;
 
   LOG_INFO() << "symbol " << arg << " is defined in " << ret.file << ":" << ret.line;
 }
